f3d11ba9acccffe6d7679eb59ff5899f473d3ed1
fernando cardenas <fernandojosecardenas20@gmail.com>
2023-04-21 17:44:41 +0200

    correciones de db variables port

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,7 @@
 
 function conectarDB() : mysqli {
 
-    $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'],$_ENV['DB_PASSWORD'], $_ENV['BD_NAME']);
+    $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'],$_ENV['DB_PASSWORD'], $_ENV['BD_NAME'], $_ENV['PORT']);
 
     if (!$db) {
         echo "Error no se pudo conectar";
